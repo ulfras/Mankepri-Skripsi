2a03737c4c939bd2d0ca3dfbc49754a3980e53a6
Maulana Frasha <ul.frasha@gmail.com>
2022-07-01 03:34:38 +0700

    Create Xcode file project, init cocoapods and install dependency, sorting and grouping the file.

---
+++ Mankepri/Mankepri/PresentationLayer/TabBarMain/TabBarMainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TabBarMainViewController.swift
+//  Mankepri
+//
+//  Created by Maulana Frasha on 01/07/22.
+//
+
+import UIKit
+
+class TabBarMainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Mankepri/Mankepri/Utility/Extension/UIViewController+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController+Extension.swift
+//  Mankepri
+//
+//  Created by Maulana Frasha on 01/07/22.
+//
+
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
